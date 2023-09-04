85d4a359f451aee60f57dcb239484482e9f7a03b
kyeongchan92 <nahcklee@gmail.com>
2023-09-04 20:02:24 +0900

    view -> model -> tamplate

--- config/views.py
+++ config/views.py
@@ -1,11 +1,21 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from burgers.models import Burger
+
 
 def main(request):
     return render(request, "main.html")
 
+
 def burger_list(request):
-    return render(request, "burger_list.html")
+    burgers = Burger.objects.all()
+    print(f"전체 햄버거 목록 : {burgers}")
+
+    context = {
+        'burgers': burgers
+    }
+    return render(request, "burger_list.html", context)
+
 
 def pseudo_recomm_research(request):
     return render(request, "index.html")
